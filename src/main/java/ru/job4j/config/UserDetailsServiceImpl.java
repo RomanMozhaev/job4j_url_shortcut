8d97a93944fdbbeaa2f889a115b35223c7496284
RomanMozhaev <roman.mozhaev@gmail.com>
2020-04-19 21:58:07 +0300

    Тестовое задание #202318

--- src/main/java/ru/job4j/config/UserDetailsServiceImpl.java
+++ src/main/java/ru/job4j/config/UserDetailsServiceImpl.java
@@ -1,14 +1,11 @@
 package ru.job4j.config;
 
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ru.job4j.domain.WebSite;
 import ru.job4j.repository.WebSiteRepository;
 
-import java.util.Collections;
 import java.util.Optional;
 
 /**
@@ -33,12 +30,11 @@ public class UserDetailsServiceImpl implements UserDetailsService {
      * @throws UsernameNotFoundException
      */
     @Override
-    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+    public WebSite loadUserByUsername(String login) throws UsernameNotFoundException {
         Optional<WebSite> site = this.repository.findByLogin(login);
         if (site.isEmpty()) {
             throw new UsernameNotFoundException(login);
         }
-
-        return new User(site.get().getLogin(), site.get().getPassword(), Collections.emptyList());
+        return site.get();
     }
 }
